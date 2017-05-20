0d3a644641dd6572a12a5888509e993354668cf7
husny_ahamed <husny.ahamed@workflowsapp.com>
2017-05-20 20:06:07 +0530

    If else inside html

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var globalApp = (function(){
+var globalApp = (function () {
     var app = new Vue({
         el: '#app',
         data: {
             message: 'Hello Vue World 2!',
-            getAuthor: function(){
+            getAuthor: function () {
                 this.author = "The Expert Husny";
                 this.name = "M.G.Husny Ahamed";
                 return this.author;
             },
             intro: 'Welcome to the vue world. <small>It is all about <b>Vue.js</b></small>',
-            isViewed: true            
+            isViewed: false            
         },
         methods: {
-            toggleView: function(){
+            toggleView: function () {
                 this.isViewed = !this.isViewed
             }
         }
     });
 
-    return app;    
+    return app;
 }())
